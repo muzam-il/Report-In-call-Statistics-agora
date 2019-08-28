695b43079a52bf608a70ad3109653c61281fdaee
Muhamed Musammil P <p.musammil98@gmail.com>
2019-08-28 13:05:57 +0530

    commit1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import RTCClient from "./rtc-client";
+import {
+  getDevices,
+  serializeFormData,
+  validator,
+  resolutions
+} from "./common";
+import "./assets/style.scss";
+import * as M from "materialize-css";
+import { setFormData, parseFromSearch } from "./searchParam";
